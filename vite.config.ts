f8471120e75ea56a435a613b7a2ae3fe138aa5e3
Haitham El Abdioui <152732703+elabdioui@users.noreply.github.com>
2025-06-06 21:55:34 +0100

    Fix vite-plugin-compression import error

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import legacy from '@vitejs/plugin-legacy';
-import { compression } from 'vite-plugin-compression';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
